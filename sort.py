60530ef24fdba4225380f8ea72830c745b3391bc
AbdullahAmir <abdullahamir615@gmail.com>
2024-04-29 16:35:30 +0500

    Sorting upload

---
+++ sort.py
@@ -0,0 +1,14 @@
+# Define a list of numbers
+numbers = [5, 2, 9, 1, 7]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print("Sorted numbers in ascending order:", sorted_numbers)
+
+# Sort the list in descending order
+sorted_numbers_desc = sorted(numbers, reverse=True)
+
+# Print the sorted list in descending order
+print("Sorted numbers in descending order:", sorted_numbers_desc)
